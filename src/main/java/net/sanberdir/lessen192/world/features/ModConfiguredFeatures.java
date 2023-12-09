fca48c5a105230913394042d5136b41fc86522cd
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2023-12-09 18:03:39 +0300

    lessen-1.19.2-11

---
+++ src/main/java/net/sanberdir/lessen192/world/features/ModConfiguredFeatures.java
@@ -0,0 +1,66 @@
+package net.sanberdir.lessen192.world.features;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.RandomSpreadFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.SpruceFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
+import net.minecraft.world.level.levelgen.feature.treedecorators.CocoaDecorator;
+import net.minecraft.world.level.levelgen.feature.treedecorators.LeaveVineDecorator;
+import net.minecraft.world.level.levelgen.feature.treedecorators.TrunkVineDecorator;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.BendingTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import net.sanberdir.lessen192.Lessen192;
+import net.sanberdir.lessen192.init.block.InitBlocksL192;
+
+import java.util.List;
+
+public class ModConfiguredFeatures {
+    public static final DeferredRegister<ConfiguredFeature<?, ?>> CONFIGURED_FEATURES =
+            DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, Lessen192.MOD_ID);
+//Конрфигуратор кастомного дерева
+    public static final RegistryObject<ConfiguredFeature<?, ?>> CUSTOM_TREE =
+            CONFIGURED_FEATURES.register("custom_tree", () ->
+                    new ConfiguredFeature<>(Feature.TREE, createBirch().build()));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> CUSTOM_SPAWN =
+            CONFIGURED_FEATURES.register("custom_spawn", () -> new ConfiguredFeature<>(Feature.RANDOM_SELECTOR,
+                    new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(
+                            ModPlacedFeatures.CUSTOM_CHECKED.getHolder().get(),
+                            0.5F)), ModPlacedFeatures.CUSTOM_CHECKED.getHolder().get())));
+    private static TreeConfiguration.TreeConfigurationBuilder createBirch() {
+        return createStraightBlobTree(InitBlocksL192.CUSTOM_LOG.get(), InitBlocksL192.CUSTOM_LEAVES.get(),
+                5, 2, 0, 2).ignoreVines();
+    }
+    private static TreeConfiguration.TreeConfigurationBuilder createStraightBlobTree(Block p_195147_, Block p_195148_, int p_195149_, int p_195150_, int p_195151_, int p_195152_) {
+        return new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(p_195147_),
+                new StraightTrunkPlacer(p_195149_, p_195150_, p_195151_), BlockStateProvider.simple(p_195148_),
+                new BlobFoliagePlacer(ConstantInt.of(p_195152_), ConstantInt.of(0), 3),
+                new TwoLayersFeatureSize(1, 0, 1));
+    }
+    //Конец конфигуратора кастомного дерева
+
+
+
+    public static void register(IEventBus eventBus) {
+        CONFIGURED_FEATURES.register(eventBus);
+    }
+}
